e8a9386c5fe3661d340916a5d5a21da3f67c5a2e
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-05 16:51:55 +0530

    Create kth largest elment in an array.java

---
+++ kth largest elment in an array.java
@@ -0,0 +1,15 @@
+//brute force approach
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]<nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums[k-1];
+    }
+}
