3eec6f593228d3f38047908a24b291526ec05499
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-05 16:26:21 +0530

    Update Repeat and missing number array.java

--- Repeat and missing number array.java
+++ Repeat and missing number array.java
@@ -20,3 +20,30 @@ public class Solution {
         return new int[]{repeating,missing};
     }
 }
+
+//optimal approach
+public class Solution {
+    public int[] repeatedNumber(final int[] A) {
+        int n = A.length;
+
+        long actualSum = 0, actualSqSum = 0;
+        for (int num : A) {
+            actualSum += num;
+            actualSqSum += (long) num * num;
+        }
+
+        long expectedSum = (long) n * (n + 1) / 2;
+        long expectedSqSum = (long) n * (n + 1) * (2 * n + 1) / 6;
+
+        long diff = expectedSum - actualSum;           
+        long sqDiff = expectedSqSum - actualSqSum;      
+
+        long sum = sqDiff / diff;                       
+
+        long missing = (diff + sum) / 2;
+        long repeating = missing - diff;
+
+        return new int[]{(int) repeating, (int) missing};
+    }
+}
+
