f8a7297c6c8c8fe6a3379e4294f951ad8580b682
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-06 23:05:06 +0530

    Update Product of array except itself.java

--- Product of array except itself.java
+++ Product of array except itself.java
@@ -15,3 +15,29 @@ class Solution {
         return ans;
     }
 }
+
+//efficient solution
+//prefix suffix approach...
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            ans[i]=1;
+        }
+        //left side
+        int left =1;
+        for(int i=0;i<n;i++){
+            ans[i] *=left;
+            left *= nums[i];
+        }
+        //right side
+        int right=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i] *= right;
+            right *= nums[i];
+        }
+        return ans;
+    }
+}
