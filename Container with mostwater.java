d4528b4698feb0c52c0578db73e77c50e33b2334
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-09 22:03:04 +0530

    Create Container with mostwater.java

---
+++ Container with mostwater.java
@@ -0,0 +1,40 @@
+//bruteforce approach...
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int maximumarea = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+               int length = Math.min(height[i],height[j]);
+               int breadth = j-i;
+               int area = length * breadth;
+               maximumarea = Math.max(area,maximumarea); 
+            }
+            
+        }
+        return maximumarea;
+    }
+}
+
+//optimal solution..
+class Solution {
+    public int maxArea(int[] height) {
+        int mostwater = 0;
+        int lp = 0;//lp = left pointer
+        int rp = height.length-1;//rp = right pointer
+
+        while(lp<rp){
+            int breadth = rp-lp;
+            int length = Math.min(height[lp],height[rp]);
+            int area = breadth * length;
+            mostwater = Math.max(area,mostwater);
+            if(height[lp]<height[rp]){
+                lp++;
+            }else{
+                rp--;
+            }
+        }
+        return mostwater;
+    }
+}
+//optimal approach by two pointer approach
