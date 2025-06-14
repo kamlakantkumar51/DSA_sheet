a52c8f142aee58da02e02674acadebcdb1e3ffb8
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-14 20:28:34 +0530

    Create Longest Common Prefix.java

---
+++ Longest Common Prefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        //taking an string from an strs and then comparing to the other strings
+        for(int i=0;i<strs[0].length();i++){
+            char ch = strs[0].charAt(i);
+            //working on other parts of strings
+            for(int j = 1;j<strs.length;j++){
+                if(i>= strs[0].length() ||strs[j].charAt(i)!= ch){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+
+    }
+}
+//vertical scanning approach
