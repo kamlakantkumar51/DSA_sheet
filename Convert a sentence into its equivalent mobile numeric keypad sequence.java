a2d2a86c2b95b6e3a5a2ced8b4e5f21495dcd22b
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-16 09:40:46 +0530

    Create Convert a sentence into its equivalent mobile numeric keypad sequence.java

---
+++ Convert a sentence into its equivalent mobile numeric keypad sequence.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+class Solution {
+    String printSequence(String S) {
+        String[] keypad = {
+            "2", "22", "222",     
+            "3", "33", "333",    
+            "4", "44", "444",   
+            "5", "55", "555",    
+            "6", "66", "666",     
+            "7", "77", "777", "7777", 
+            "8", "88", "888",     
+            "9", "99", "999", "9999"  
+        };
+        StringBuilder result = new StringBuilder();
+        
+        for(int i=0;i<S.length();i++){
+            char ch = S.charAt(i);
+            if(ch == ' '){
+                result.append("0");
+            }else{
+                int pos = ch-'A';
+                result.append(keypad[pos]);
+            }
+        }
+        return result.toString();
+    }
+}
