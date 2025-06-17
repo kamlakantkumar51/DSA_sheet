5fbc6e7f2d53ade7fa8c00891a1c9b1f76f12504
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-17 22:53:08 +0530

    Create Longest Repeating Character Replacement.java

---
+++ Longest Repeating Character Replacement.java
@@ -0,0 +1,47 @@
+//bruteforce approach
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int n = s.length();
+        int maxlength = 0;
+
+        for(int i=0;i<n;i++){
+            int freq[] = new int[26];
+            int maxfreq = 0;
+            for(int j = i;j<n;j++){
+                freq[s.charAt(j)-'A']++;
+                maxfreq = Math.max(maxfreq,freq[s.charAt(j)-'A']);
+
+                int lengthofsubstr = j-i+1;
+                int replacingcharac = lengthofsubstr - maxfreq;
+
+                if(replacingcharac <= k){
+                    maxlength = Math.max(maxlength,lengthofsubstr);
+                }
+            }
+        }
+        return maxlength;
+    }
+}
+//optimal solution
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int maxFreq = 0;
+        int start = 0;
+        int ans = 0;
+        for (int end = 0; end < s.length(); end++) {
+            freq[s.charAt(end) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(end) - 'A']);
+
+            int windowSize = end - start + 1;
+            int toReplace = windowSize - maxFreq;
+
+            if (toReplace > k) {
+                freq[s.charAt(start) - 'A']--; 
+                start++;
+            }
+            ans = Math.max(ans, end - start + 1);
+        }
+        return ans;
+    }
+}
