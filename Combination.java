a2a083e3423c3d14a462654e0bc7897d95eb8c65
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-18 09:08:08 +0530

    Update Combination.java

--- Combination.java
+++ Combination.java
@@ -1,3 +1,8 @@
+// Input: n = 4, k = 2
+// Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+// Explanation: There are 4 choose 2 = 6 total combinations.
+// Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+// Notice that the order of the output and the order of the triplets does not matter.
 import java.util.*;
 class Solution {
     public void combination(int index, int k, List<Integer> data, List<List<Integer>> result, int n) {
