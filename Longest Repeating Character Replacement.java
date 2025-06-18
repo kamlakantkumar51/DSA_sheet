e7b26a3aaf06d2ae59f6060eb682274d1fa5377f
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-06-18 10:05:45 +0530

    Update Longest Repeating Character Replacement.java

--- Longest Repeating Character Replacement.java
+++ Longest Repeating Character Replacement.java
@@ -1,3 +1,6 @@
+// Input: s = "ABAB", k = 2
+// Output: 4
+// Explanation: Replace the two 'A's with two 'B's or vice versa.
 //bruteforce approach
 class Solution {
     public int characterReplacement(String s, int k) {
