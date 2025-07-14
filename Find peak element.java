ca4e06006c8b2416cdf1c6dfabfb5ec98eba06c2
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-07-14 17:32:36 +0530

    Update Find peak element.java

--- Find peak element.java
+++ Find peak element.java
@@ -1,4 +1,4 @@
-//my approach coe
+//my approach code
 class Solution {
     public int findPeakElement(int[] nums) {
         for(int i=0;i<nums.length-1;i++){
@@ -10,7 +10,7 @@ class Solution {
     }
 }
 
-//bruteforce approach
+// my bruteforce approach
 class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
