7b99a4c7df9dd8e0c154da58a66b2a3d87da9f81
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-08-23 21:41:28 +0530

    Create sortList.java

---
+++ sortList.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode mid = middlenode(head);
+        ListNode rightpart = mid.next;
+        mid.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightpart);
+
+        return merge(left,right);
+    }
+    private ListNode middlenode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!= null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(l1!= null && l2!= null){
+            if(l1.val<l2.val){
+                tail.next = l1;
+                l1 = l1.next;
+            }else{
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;  
+        }
+        if(l1!= null) tail.next = l1;
+        if(l2 != null) tail.next = l2;
+        return dummy.next;
+    }
+}
