0417fd218cccbd302a1c03d838b50d2cee20410f
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-01-16 15:07:26 +0530

    Create Reversepolishnotation.java

---
+++ Reversepolishnotation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+
+        for(String s:tokens){
+            if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")){
+                int b = stack.pop();
+                int a = stack.pop();
+
+                if(s.equals("+"))stack.push(a+b);
+                else if(s.equals("-"))stack.push(a-b);
+                else if(s.equals("*")) stack.push(a*b);
+                else stack.push(a/b);
+            }else{
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
