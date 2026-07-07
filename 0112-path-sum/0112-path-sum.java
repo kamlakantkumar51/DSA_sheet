572c660f17e2da1d5549a9e50e08489e64522223
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-07-07 22:16:20 +0530

    Time: 0 ms (100%), Space: 44.8 MB (68.29%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -3,12 +3,11 @@ class Solution {
         return helper(root,0,targetSum);
     }
     private boolean helper(TreeNode root,int sum,int target){
-        if(root == null){
-            return false;
-        }
+        if(root == null)return false;
         sum += root.val;
+
         if(root.left == null && root.right == null){
-            return target == sum;
+            return sum == target;
         }
         return helper(root.left,sum,target) || helper(root.right,sum,target);
     }
