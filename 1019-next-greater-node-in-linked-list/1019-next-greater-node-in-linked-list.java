552b9a5663ede5f17ddde7466dabb89f07712d8c
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-07-11 15:48:38 +0530

    Time: 24 ms (55.18%), Space: 50.7 MB (6.53%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -0,0 +1,22 @@
+ class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        while(head != null){
+            nums.add(head.val);
+            head = head.next;
+        }
+        int n = nums.size();
+        int ans[] = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=nums.get(i))
+            stack.pop();
+            if(!stack.isEmpty()){
+                ans[i] = stack.peek();
+            }
+            stack.push(nums.get(i));
+        }
+        return ans;
+    }
+}
