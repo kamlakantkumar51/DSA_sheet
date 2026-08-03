e7a6a8180f05fe8f59b9d6e71fd3286ba7502879
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-08-03 12:33:45 +0530

    Time: 0 ms (100%), Space: 43.1 MB (75.9%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -1,21 +1,24 @@
+//dfs approach 
 class Solution {
-    int depthX = -1;
-    int depthY = -1;
-    TreeNode parentX = null;
-    TreeNode parentY = null;
+    TreeNode xParent = null;
+    TreeNode yParent = null;
+    int xDepth = -1;
+    int yDepth = -1;
     public boolean isCousins(TreeNode root, int x, int y) {
         dfs(root,null,0,x,y);
-        return depthX == depthY && parentX!= parentY;
+        return xDepth == yDepth && xParent!= yParent;
     }
     private void dfs(TreeNode node,TreeNode parent,int depth,int x,int y){
-        if(node == null)return;
+        if(node == null){
+            return;
+        }
         if(node.val == x){
-            depthX = depth;
-            parentX = parent;
+            xParent = parent;
+            xDepth = depth;
         }
         if(node.val == y){
-            depthY = depth;
-            parentY = parent;
+            yParent = parent;
+            yDepth = depth;
         }
         dfs(node.left,node,depth+1,x,y);
         dfs(node.right,node,depth+1,x,y);
