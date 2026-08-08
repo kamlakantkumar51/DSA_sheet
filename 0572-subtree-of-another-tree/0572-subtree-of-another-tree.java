1e34f8a0e80261a156d5d6751a4269c5b9c642bf
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-08-08 19:22:22 +0530

    Time: 7 ms (8%), Space: 46.4 MB (52.81%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -3,27 +3,23 @@ class Solution {
         if(root == null){
             return false;
         }
-        if(root.val == subRoot.val){
-            if(isIdentical(root,subRoot)){
-                return true;
-            }
-        }
-        boolean leftans = isSubtree(root.left,subRoot);
-        boolean rightans = isSubtree(root.right,subRoot);
-        return leftans || rightans;
-    }
-    private static boolean isIdentical(TreeNode root,TreeNode subRoot){
-        if(root == null && subRoot == null){
+        if(isSameTree(root,subRoot)){
             return true;
-        }else if(root == null || subRoot == null || root.val != subRoot.val){
-            return false;
         }
-        if(!isIdentical(root.left,subRoot.left)){
-            return false;
-        }
-        if(!isIdentical(root.right,subRoot.right)){
-            return false;
-        }
-        return true;
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
     }
+    private boolean isSameTree(TreeNode p,TreeNode q){
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
+    }
+
 }
