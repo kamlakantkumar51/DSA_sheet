a25c924ff92c1c704ee7f7eeae02a001f9ea5133
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-08-08 18:42:14 +0530

    Time: 3 ms (94.94%), Space: 44 MB (68.68%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,35 @@
+//dfs
+class Solution {
+    List<int[]>list = new ArrayList<>();
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        dfs(root,0,0);
+        Collections.sort(list,(a,b)->{
+            if(a[0]!= b[0]){
+                return a[0]-b[0];
+            }
+            if(a[1]!= b[1]){
+                return a[1]-b[1];
+            }
+            return a[2]-b[2];
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        int prevcol = Integer.MIN_VALUE;
+        for(int node[]:list){
+            if(node[0]!= prevcol){
+                ans.add(new ArrayList<>());
+                prevcol = node[0];
+            }
+            ans.get(ans.size()-1).add(node[2]);
+        }
+        return ans;
+    }
+    private void dfs(TreeNode root,int row,int col){
+        if(root == null){
+            return;
+        }
+        list.add(new int[]{col,row,root.val});
+
+        dfs(root.left,row+1,col-1);
+        dfs(root.right,row+1,col+1);
+    }
+}
