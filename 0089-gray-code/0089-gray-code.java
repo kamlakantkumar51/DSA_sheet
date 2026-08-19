18e083fef7ff2b3ae656980c164a65718736b16a
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-08-19 14:44:37 +0530

    Time: 5 ms (54.05%), Space: 53.3 MB (91.34%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+        int totalNumbers = 1 << n; 
+        
+        for (int i = 0; i < totalNumbers; i++) {
+            ans.add(i ^ (i >> 1));
+        }
+        return ans;
+    }
+}
