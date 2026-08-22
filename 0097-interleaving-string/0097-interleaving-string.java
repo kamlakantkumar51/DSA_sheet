fc30ad80efa36e02b3d71b4fad2f99d4812d3713
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-08-22 14:28:22 +0530

    Time: 5 ms (38.53%), Space: 43.3 MB (53.29%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        if( m+n != s3.length())return false;
+        boolean dp[] = new boolean[n+1];
+        dp[0] = true;
+        //initialization
+        for(int j=1;j<=n;j++){
+            dp[j] = dp[j-1] && s2.charAt(j-1) == s3.charAt(j-1);
+        }
+        for(int i=1;i<=m;i++){
+            dp[0] = dp[0] && s1.charAt(i-1) == s3.charAt(i-1);
+            for(int j=1;j<=n;j++){
+                char currents3 = s3.charAt(i+j-1);
+                boolean matchs1 = dp[j] && s1.charAt(i-1) == currents3;
+                boolean matchs2 = dp[j-1] && s2.charAt(j-1) == currents3;
+                dp[j] = matchs1 || matchs2;
+            }
+        }
+        return dp[n];
+    }
+}
