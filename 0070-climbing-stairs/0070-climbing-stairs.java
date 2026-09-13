fb1db184b211102d3f9d373bb27e80f713c91d3c
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-09-13 21:54:20 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,20 +3,20 @@ class Solution {
     public int climbStairs(int n) {
         int ways[] = new int[n+1];
         Arrays.fill(ways,-1);
-        int ans = totalWays(n,ways);
+        int ans = totalways(n,ways);
         return ans;
     }
-    private static int totalWays(int n,int ways[]){
+    private int totalways(int n,int ways[]){
         if(n == 0){
             return 1;
         }
-        if(n < 0){
+        if(n < 0 ){
             return 0;
         }
-        if(ways[n]!= -1){
+        if(ways[n] != -1){
             return ways[n];
         }
-        ways[n] =  totalWays(n-1,ways)+totalWays(n-2,ways);
+        ways[n] = totalways(n-1,ways)+totalways(n-2,ways);
         return ways[n];
     }
 }
